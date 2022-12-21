6af301e0b6903f4d9ced539776045f433ef7c802
Alex Kelly <alexjohnfranciskelly@gmail.com>
2022-12-21 12:41:35 +0000

    Update export-commits-to-csv-for-a-repo.sh
    Probably better to do this using the GitHub CLI since we can paginate through all results easily.

--- bash-scripts/export-commits-to-csv-for-a-repo.sh
+++ bash-scripts/export-commits-to-csv-for-a-repo.sh
@@ -17,10 +17,7 @@ GITHUB_TOKEN=
 echo "Author,Email,Date,Committer,Committer Email,Committer Date,Message,HTML_URL,Comment Count,Verification reason" > output.csv
 
 # Finally, we'll make an API request to retrieve the commits for the repository
-curl \
-     -H "Accept: application/vnd.github.v3+json" \
-     -H "Authorization: token ${GITHUB_TOKEN}" \
-     https://api.github.com/repos/${ORG}/${REPO}/commits |
+gh api --paginate -H "Accept: application/vnd.github.v3+json" -H "Authorization: token ${GITHUB_TOKEN}" /repos/${ORG}/${REPO}/commits |
 
 # Use jq to extract the specified values from the JSON response and create a CSV file
 jq -r '.[] | [.commit.author.name, .commit.author.email, .commit.author.date, .commit.committer.name, .commit.committer.email, .commit.committer.date, .commit.message, .html_url, .commit.comment_count, .commit.verification.reason] | @csv' >> output.csv
